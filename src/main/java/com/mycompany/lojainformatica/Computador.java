a5583d3048af82963ac9052623e4561fd08789c1
pedrootsugua <pedro.zanele@gmail.com>
2023-11-18 15:26:43 -0300

    feat: class computador

--- src/main/java/com/mycompany/lojainformatica/Computador.java
+++ src/main/java/com/mycompany/lojainformatica/Computador.java
@@ -9,5 +9,40 @@ package com.mycompany.lojainformatica;
  * @author pedro
  */
 public class Computador {
-    
+    private static String marca = "Pedro Augusto";
+    private String HD;
+    private String processador;
+
+    public Computador() {
+    }
+
+    public Computador(String HD, String processador) {
+        this.HD = HD;
+        this.processador = processador;
+    }
+
+    public static String getMarca() {
+        return marca;
+    }
+
+    public static void setMarca(String marca) {
+        Computador.marca = marca;
+    }
+
+    public String getHD() {
+        return HD;
+    }
+
+    public void setHD(String HD) {
+        this.HD = HD;
+    }
+
+    public String getProcessador() {
+        return processador;
+    }
+
+    public void setProcessador(String processador) {
+        this.processador = processador;
+    }
+
 }
